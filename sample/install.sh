754f9a4e01cc4e88e74d8a9e7a122c93b706b65d
Sebastian Schindler <sschindler@tpip.net>
2015-11-04 13:35:18 +0100

    added unit with timer to regularly delete log files
    * this was needed because systemd-journal-remote
      doesn't rotate the logs properly

--- sample/install.sh
+++ sample/install.sh
@@ -2,4 +2,7 @@
 
 cp -f journal-gateway-zmtp-control journal-gateway-zmtp-sink journal-gateway-zmtp-source /usr/bin/
 cp -f ./misc/journal-gateway-zmtp-s*.service /etc/systemd/system/
+cp -f ./remove_old_logs.service /etc/systemd/system/
+cp -f ./remove_old_logs.timer /etc/systemd/system/
 cp -f ./misc/journal-gateway-zmtp-s*.conf /etc
+cp -f ./misc/remove_old_logs.conf /etc
