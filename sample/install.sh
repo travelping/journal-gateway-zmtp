b4c6123d5c303975f2d6402dafd941592d7fdeba
Sebastian Schindler <sschindler@tpip.net>
2015-11-04 15:02:29 +0100

    rename remove_old* -> rotate

--- sample/install.sh
+++ sample/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 cp -f journal-gateway-zmtp-control journal-gateway-zmtp-sink journal-gateway-zmtp-source /usr/bin/
-cp -f ./misc/journal-gateway-zmtp-s*.service /etc/systemd/system/
-cp -f ./remove_old_logs.service /etc/systemd/system/
-cp -f ./remove_old_logs.timer /etc/systemd/system/
+cp -f ./misc/journal-gateway-zmtp-s*.service /lib/systemd/system/
+cp -f ./rotate_logs.service /lib/systemd/system/
+cp -f ./rotate_logs.timer /lib/systemd/system/
 cp -f ./misc/journal-gateway-zmtp-s*.conf /etc
-cp -f ./misc/remove_old_logs.conf /etc
