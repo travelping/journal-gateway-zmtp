3c344a599a092d48e28e3f139f0e89c158cdb06a
Sebastian Schindler <sschindler@tpip.net>
2016-01-14 13:25:31 +0100

    fix install.sh
    location of rotate_logs.* was incorrect

--- sample/install.sh
+++ sample/install.sh
@@ -2,6 +2,6 @@
 
 cp -f journal-gateway-zmtp-control journal-gateway-zmtp-sink journal-gateway-zmtp-source /usr/bin/
 cp -f ./misc/journal-gateway-zmtp-s*.service /lib/systemd/system/
-cp -f ./rotate_logs.service /lib/systemd/system/
-cp -f ./rotate_logs.timer /lib/systemd/system/
+cp -f ./misc/rotate_logs.service /lib/systemd/system/
+cp -f ./misc/rotate_logs.timer /lib/systemd/system/
 cp -f ./misc/journal-gateway-zmtp-s*.conf /etc
